40ddf29792ad6b29a61259d97b14a1a192d72888
Rafael <stephany@Rafaels-Air.fios-router.home>
2020-11-27 20:53:12 -0500

    uploading final routes to app.py

--- Instructions/app.py
+++ Instructions/app.py
@@ -7,8 +7,6 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
-
-
 #################################################
 # Database Setup
 #################################################
@@ -24,13 +22,10 @@ Base.prepare(engine, reflect=True)
 # Save reference to the table
 Measurement = Base.classes.measurement
 Station = Base.classes.station
-
 #################################################
 # Flask Setup
 #################################################
 app = Flask(__name__)
-
-
 #################################################
 # Flask Routes
 #################################################
@@ -74,7 +69,6 @@ def precipitation():
     # jsonify the results
     return jsonify(prcp)
 
-
 @app.route("/api/v1.0/stations")
 def stations():
     # Create our session (link) from Python to the DB
@@ -90,12 +84,96 @@ def stations():
 
     # Empty list to add the dictionary values to.
     station = []
-    for name in active_station:
+    for name, frequency in active_station:
         station_dict = {}
-        station_dict['Name'] = name
+        station_dict['Station'] = name
+        station_dict['Frequency'] = frequency
         station.append(station_dict)
 
     return jsonify(active_station)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+     # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query
+    USC00519281_tobs = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= '2016-08-23',Measurement.station == "USC00519281").\
+        group_by(Measurement.date).\
+        order_by(Measurement.date).all()
+
+    # Closing the session
+    session.close()
+
+    # Empty list to add the dictionary values to.
+    tob = []
+    for date, temp in USC00519281_tobs:
+        station_tobs_dict ={}
+        station_tobs_dict['Date'] = date
+        station_tobs_dict['Temperature'] = temp
+        tob.append(station_tobs_dict)
+
+    # jsonify the results
+    return jsonify(tob)
+
+@app.route("/api/v1.0/<start>")
+def temp(start):
+     # # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Session Query
+    temperature = session.query(func.min(Measurement.tobs),func.max(Measurement.tobs),func.avg(Measurement.tobs)).\
+        filter(Measurement.date >= start).all()
+
+    # Closing the session
+    session.close()
+
+    # Empty list to add the dictionary values to.
+    temperature_list = []
+    for min,max,avg in temperature:
+        temperature_dict = {}
+        temperature_dict['Min'] = min
+        temperature_dict['Max'] = max
+        temperature_dict['Average'] = avg
+        temperature_list.append(temperature_dict)
+
+    # UserError message    
+    if temperature_list[0]['Min'] is None:
+        return "Oops. Something went wrong. Is your date in the YYYY-MM-DD format?<br/>" \
+            "Please try again"
+
+    # jsonify the results
+    return jsonify(temperature_list)
+
+@app.route("/api/v1.0/<start>/<end>")
+def temps(start, end):
+
+     # # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    #Query
+    temperature_range = session.query(func.min(Measurement.tobs),func.max(Measurement.tobs),func.avg(Measurement.tobs)).\
+        filter(Measurement.date >= start,Measurement.date <= end).all()
+
+    # Closing the session
+    session.close()
+
+    # Empty list to add the dictionary values to.
+    temp_range_list = []
+    for min,max,avg in temperature_range:
+        temp_range_dict = {}
+        temp_range_dict['Min'] = min
+        temp_range_dict['Max'] = max
+        temp_range_dict['Average'] = avg
+        temp_range_list.append(temp_range_dict)
+
+    # Error message
+    if temp_range_list[0]['Min'] is None:
+        return "Oops. Something went wrong. Is your date in the YYYY-MM-DD format?<br/>" \
+            "Please try again"
+
+    # jsonify the results
+    return jsonify(temp_range_list)    
 
 #Add Debugger to run the app
 if __name__ == '__main__':
